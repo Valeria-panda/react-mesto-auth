728d11df836588da0b9880099b44b2edba2cbb04
Valeriya Zolotaryova <Panda.lera@mail.ru>
2021-01-24 22:52:36 +0500

    update utils

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,11 +2,11 @@ import BadRequestError from '../errors/badRequestError';
 import NotAuthorizedError from '../errors/notAuthorizedError';
 
 // export const baseUrl = 'https://auth.nomoreparties.co';
-export const baseUrl = 'https://www.lera.students.nomoredomains.rocks';
 
-// запрос на регистрацию
+export const BASE_URL = 'http://www.api.lera.students.nomoredomains.rocks';
 
-export const register = (password, email) => fetch(`${baseUrl}/signup`, {
+// Отправляем запрос на регистрацию
+export const register = (password, email) => fetch(`${BASE_URL}/signup`, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
@@ -27,47 +27,45 @@ export const register = (password, email) => fetch(`${baseUrl}/signup`, {
     return res.json();
   });
 
-// Отправляем запрос за авторизацию
-
-  export const authorize = (password, email) => fetch(`${baseUrl}/signin`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ password, email }),
+// Отправляем запрос на авторизацию
+export const authorize = (password, email) => fetch(`${BASE_URL}/signin`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ password, email }),
+})
+  .then((res) => {
+    if (res.status === 400) {
+      throw new BadRequestError('Не передано одно из полей');
+    }
+    else if (res.status === 401) {
+      throw new NotAuthorizedError('Пользователь с таким email не найден');
+    }
+    return res.json();
   })
-    .then((res) => {
-      if (res.status === 400) {
-        throw new BadRequestError('Не передано одно из полей');
-      }
-      else if (res.status === 401) {
-        throw new NotAuthorizedError('Пользователь с таким email не найден');
-      }
-      return res.json();
-    })
-    .then((data) => {
-      if (data.token) {
-        localStorage.setItem('jwt', data.token);
-        return data.token;
-      }
-    });
+  .then((data) => {
+    if (data.token) {
+      localStorage.setItem('jwt', data.token);
+      return data.token;
+    }
+  });
 
-// Отправляем запрос за получение токена
-
-  export const getContent = (token) => fetch(`${baseUrl}/users/me`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  })
-    .then((res) => {
-      if (!res.ok) {
-        return res.json()
-          .then((err) => {
-            throw new NotAuthorizedError(err.message);
-          });
-      }
+// Отправляем запрос на получение токена
+export const getContent = (token) => fetch(`${BASE_URL}/users/me`, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+  },
+})
+  .then((res) => {
+    if (!res.ok) {
       return res.json()
-    })
-    .then((data) => data);
+        .then((err) => {
+          throw new NotAuthorizedError(err.message);
+        });
+    }
+    return res.json()
+  })
+  .then((data) => data);
